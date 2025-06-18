6ed864bf8bd4d69449f3e2648af1b2c1f652b653
Omair <al3dwii@outlook.com>
2025-06-19 01:17:44 +0300

    fix frontend 1

--- web/.storybook/main.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  stories: ['../src/components/**/*.stories.@(ts|tsx)'],
-  addons: ['@storybook/addon-essentials', '@storybook/addon-a11y'],
-  framework: '@storybook/react',
-};
